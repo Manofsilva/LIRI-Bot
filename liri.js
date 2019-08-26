9303f2a7f447dba060902235a874d17fb90d4bd5
Cesar Silva <silvacde@gmail.com>
2019-08-25 22:29:32 -0700

    added Ace of Base

--- liri.js
+++ liri.js
@@ -17,15 +17,15 @@ var spotify = new Spotify(keys.spotify);
 inquirer
     .prompt([
         {
-        type: "list",
-        message: "What Would You Like To Ask About: Concerts, Spotify Songs, Movies?",
-        choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"],
-        name: "choice"
+            type: "list",
+            message: "What Would You Like To Ask About: Concerts, Spotify Songs, Movies?",
+            choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"],
+            name: "choice"
         },
     ])
     // concert this
     .then(function (res) {
-        if (res.choice === "concert-this"){
+        if (res.choice === "concert-this") {
             console.log("Welcome");
             // Ask about an artist
             inquirer
@@ -40,26 +40,55 @@ inquirer
                     // calling Bands in Town API
                     let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
                     // getting results from API
-                    if(result.artist == ""){
+                    if (result.artist == "") {
                         console.log("Please, enter an artist");
                     } else {
                         axios.get(queryUrl).then(
                             function (response) {
                                 // loop through the response data info
-                                for (let i = 0; i <response.data.length; i++) {
+                                for (let i = 0; i < response.data.length; i++) {
                                     // convert date with moment()
                                     let date = moment(response.data[i].datetime).format('MM/DD/YYYY')
                                     console.log("\n=================");
-                  console.log("Venue: " + response.data[i].venue.name);
-                  console.log("City: " + response.data[i].venue.city);
-                  console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
-                  console.log("=================");
-                }
-                                // append artist to log.txt
+                                    console.log("Venue: " + response.data[i].venue.name);
+                                    console.log("City: " + response.data[i].venue.city);
+                                    console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
+                                    console.log("=================");
+                                }
                             }
                         )
                     }
                 })
+        } else if (res.choice === "spotify-this-song") {
+            console.log("\n=================");
+            console.log("Welcome");
+            console.log("\n=================");
+            //asking about Spotify
+            inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        message: "What Track Interests You?",
+                        name: "track"
+                    }
+                ])
+                .then(function (result) {
+                    if (result.track === "") {
+                        result.track = "The Sign, Ace of Base";
+                        spotify
+                            .search({ type: 'track', query: result.track, limit: 10 })
+                            .then(function (response) {
+                                console.log("\n=================");
+                                console.log("\n=================");
+                                console.log("Artist: " + response.tracks.items[0].artists[0].name);
+                                console.log("Song: " + response.tracks.items[0].name);
+                                console.log("URL: " + response.tracks.items[0].preview_url);
+                                console.log("Album: " + response.tracks.items[0].album.name);
+                                console.log("\n=================");
+                            })
+                    }
+                })
+
         }
     })
 
